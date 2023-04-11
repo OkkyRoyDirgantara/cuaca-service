67672320415d47875320eb016dc9cc667ede6944
Okky Roy Dirgantara <okkyroydirgantara@gmail.com>
2023-04-12 02:58:09 +0700

    fix bug and add dotenv

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import pytz
 import requests
 import schedule as schedule
 from bs4 import BeautifulSoup as bs
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def get_weather(time):
@@ -55,55 +57,64 @@ def save_weather():
     now = datetime.now(nowTZ)
     strNow = now.strftime("%Y-%m-%d %H:%M:%S")
 
-    weatherToday = get_weather(now)
+
     parseNow = now.strftime("%Y%m%d")
     next_run = schedule.next_run()
     print("Jadwal berikutnya dijalankan pada", next_run)
 
-    # Cek apakah 2 jam ke depan berada pada hari selanjutnya
-    two_hours_later = now + timedelta(hours=2)
-    if two_hours_later.date() > now.date():
-        weatherTomorrow = get_weather(two_hours_later)
-        sql_check = f"SELECT * FROM weathers WHERE datetime = {two_hours_later.strftime('%Y%m%d')}"
-        checkifsame = query_all(sql_check)
-        if checkifsame != []:
-            if checkifsame[0][1] != two_hours_later.strftime("%Y%m%d"):
-                sql = "INSERT INTO weathers (datetime, h0, h6, h12, h18, created_at) VALUES (%s, %s, %s, %s, %s, %s)"
-                val = (two_hours_later.strftime("%Y%m%d"), weatherTomorrow[0], weatherTomorrow[1], weatherTomorrow[2],
-                       weatherTomorrow[3], two_hours_later)
-                try:
-                    query_db(sql, val)
-                    print('inserted')
-                except Exception as e:
-                    print(f"error : {e}")
+    two_hours_next_day = now + timedelta(hours=2)
 
-    sql_check = f"SELECT * FROM weathers WHERE datetime = {parseNow}"
-    checkifsame = query_all(sql_check)
-    if checkifsame != []:
-        for x in checkifsame:
-            date = x[1]
-            if date == parseNow:
-                sql = "UPDATE weathers SET h0 = %s, h6 = %s, h12 = %s, h18 = %s, updated_at = %s WHERE datetime = %s"
-                val = (weatherToday[0], weatherToday[1], weatherToday[2], weatherToday[3], strNow, parseNow)
-                try:
-                    query_db(sql, val)
-                    print('updated')
-                except Exception as e:
-                    print(f"error : {e}")
-            else:
-                sql = "INSERT INTO weathers (datetime, h0, h6, h12, h18, created_at) VALUES (%s, %s, %s, %s, %s, %s)"
-                val = (parseNow, weatherToday[0], weatherToday[1], weatherToday[2], weatherToday[3], strNow)
-                try:
-                    query_db(sql, val)
-                    print('inserted')
-                except Exception as e:
-                    print(f"error : {e}")
-    else:
+    sql_check_today = f"SELECT * FROM weathers WHERE datetime = {parseNow}"
+    checkifsametoday = query_all(sql_check_today)
+
+    sql_check_tomorrow = f"SELECT * FROM weathers WHERE datetime = {two_hours_next_day.strftime('%Y%m%d')}"
+    checkifsametomorrow = query_all(sql_check_tomorrow)
+    if two_hours_next_day.strftime("%Y%m%d") != parseNow:
+        if checkifsametomorrow == []:
+            weatherTomorrow = get_weather(two_hours_next_day)
+            sql = "INSERT INTO weathers (datetime, h0, h6, h12, h18, created_at) VALUES (%s, %s, %s, %s, %s, %s)"
+            val = (two_hours_next_day.strftime("%Y%m%d"), weatherTomorrow[0], weatherTomorrow[1], weatherTomorrow[2],
+                   weatherTomorrow[3], strNow)
+            try:
+                query_db(sql, val)
+                print('inserted when tomorrow')
+            except Exception as e:
+                print(f"error : {e}")
+        elif checkifsametomorrow[0][1] == two_hours_next_day.strftime("%Y%m%d"):
+            weatherTomorrow = get_weather(two_hours_next_day)
+            sql = "UPDATE weathers SET h0 = %s, h6 = %s, h12 = %s, h18 = %s, updated_at = %s WHERE datetime = %s"
+            val = (weatherTomorrow[0], weatherTomorrow[1], weatherTomorrow[2], weatherTomorrow[3], strNow, two_hours_next_day.strftime("%Y%m%d"))
+            try:
+                query_db(sql, val)
+                print('updated when tomorrow')
+            except Exception as e:
+                print(f"error : {e}")
+    elif checkifsametoday != []:
+        if checkifsametoday[0][1] == parseNow:
+            weatherToday = get_weather(now)
+            sql = "UPDATE weathers SET h0 = %s, h6 = %s, h12 = %s, h18 = %s, updated_at = %s WHERE datetime = %s"
+            val = (weatherToday[0], weatherToday[1], weatherToday[2], weatherToday[3], strNow, parseNow)
+            try:
+                query_db(sql, val)
+                print('updated when not null and same date')
+            except Exception as e:
+                print(f"error : {e}")
+        elif checkifsametoday[0][1] != parseNow:
+            weatherToday = get_weather(now)
+            sql = "INSERT INTO weathers (datetime, h0, h6, h12, h18, created_at) VALUES (%s, %s, %s, %s, %s, %s)"
+            val = (parseNow, weatherToday[0], weatherToday[1], weatherToday[2], weatherToday[3], strNow)
+            try:
+                query_db(sql, val)
+                print('inserted when not null')
+            except Exception as e:
+                print(f"error : {e}")
+    elif checkifsametoday == []:
+        weatherToday = get_weather(now)
         sql = "INSERT INTO weathers (datetime, h0, h6, h12, h18, created_at) VALUES (%s, %s, %s, %s, %s, %s)"
         val = (parseNow, weatherToday[0], weatherToday[1], weatherToday[2], weatherToday[3], strNow)
         try:
             query_db(sql, val)
-            print('inserted')
+            print('inserted when null')
         except Exception as e:
             print(f"error : {e}")
 
